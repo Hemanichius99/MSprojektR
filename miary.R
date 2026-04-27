e5e6d405f4199b6f7497e3fd3fa48b2907c25026
Marcel Zagdan (mz316207) <mz316207@student.polsl.pl>
2026-04-27 15:00:07 +0200

    fix zad 2 warning

--- miary.R
+++ miary.R
@@ -2,27 +2,17 @@
 
 f_sr = function(x)
 {
-  bb = seq(min(x), max(x), length.out = 8)  # wektor wartośći od min do max równoodległych od siebie i podzielonych na 8 części
-  hh = hist(x, bb, xlim = c(10, 35), labels = TRUE, freq = FALSE, plot = FALSE)
+  bb = seq(min(x), max(x), length.out = 8)
+  hh = hist(x, breaks = bb, plot = FALSE)
+  return(hh)
 }
-
-
-
-
-
-
-
-
-
 
 #funkcja zwracająca szereg rozdzielczy
 wart_sr = function (wektor) {
   sr_wektor = f_sr(wektor)
   
-  
   # wartość średnia szereg rozdzielczy
   xsr_sr1 = sum(sr_wektor$mids * sr_wektor$counts) / sum(sr_wektor$counts);
-  
   
   # wariancja nieobciążona s2*szereg rozdzielczy
   var_sr1 = sum(sr_wektor$counts * (sr_wektor$mids - xsr_sr1)^2) / (sum(sr_wektor$counts) - 1)
@@ -36,10 +26,6 @@ wart_sr = function (wektor) {
   
   # odchylenie standardowe s szereg rozdzielczy
   os_sr1_r_n = sqrt(var_sr1_n)
-  
-  
-  
-  
   
   # kwartyle szereg rozdzielczy
   kwartyl_sr <- function(sr, p) {
@@ -62,7 +48,6 @@ wart_sr = function (wektor) {
   m_sr1  = kwartyl_sr(sr_wektor, 0.5)
   q3_sr1 = kwartyl_sr(sr_wektor, 0.75)
   
-  
   # moda szereg rozdzielczy
   moda_sr <- function(sr) {
     i <- which.max(sr$counts)
@@ -81,13 +66,13 @@ wart_sr = function (wektor) {
   moda_sr1 = moda_sr(sr_wektor)
   
   # skośność szereg rozdzielczy
-  sk_sr1 = sum((sr_wektor$mids - xsr_sr1)^3) / (length(wektor) * os_sr1_r^3)
-  
+  sk_sr1 = sum(((sr_wektor$mids - xsr_sr1)^3) * sr_wektor$counts) / (sum(sr_wektor$counts) * os_sr1_r_n^3)
   
   # kurtoza szereg rozdzielczy
-  kur_sr1 = sum((sr_wektor$mids - xsr_sr1)^4) / (length(wektor) * os_sr1_r^4)
+  kur_sr1 = sum(((sr_wektor$mids - xsr_sr1)^4) * sr_wektor$counts) / (sum(sr_wektor$counts) * os_sr1_r_n^4)
   
-  
+  # eksces szereg rozdzielczy
+  ex_sr1 = kur_sr1 - 3
   
   # odchylenie przeciętne od średniej szereg rozdzielczy
   d1_sr1 = sum(abs(sr_wektor$mids - xsr_sr1) * sr_wektor$counts) / sum(sr_wektor$counts)
@@ -95,77 +80,59 @@ wart_sr = function (wektor) {
   # odchylenie przeciętne od mediany szereg rozdzielczy
   d2_sr1 = sum(abs(sr_wektor$mids - m_sr1) * sr_wektor$counts) / sum(sr_wektor$counts)
   
-  
   # odchylenie ćwiartkowe szereg rozdzielczy
   Q_sr1 = (q3_sr1 - q1_sr1) / 2
   
   # współczynnik zmienności szereg rozdzielczy
-  v_sr1 = (os_sr1_r / xsr_sr1) * 100
+  v_sr1 = (os_sr1_r_n / xsr_sr1) * 100
   
   # pozycyjny współczynnik zmienności szereg rozdzielczy
-  vq_sr1 = ((q3_sr1 - q1_sr1) / (2 * m_sr1)) * 100
+  vq_sr1 = (Q_sr1 / m_sr1) * 100
   
-  #srednia, mediana, moda, q1, q3, wariancja*, odch stan *,wariancja, odch stand, odch przec, odch przec od mediany,odch ćwiart, wsp zmienności, poz wsp zmiennosci, skosnosc, kurtoza, eksces                                    
   wynik_sr = c(
-    xsr_sr1, m_sr1, moda_sr1, q1_sr1, q3_sr1, var_sr1,  os_sr1_r, var_sr1_n, os_sr1_r_n,d1_sr1, d2_sr1, Q_sr1, v_sr1, vq_sr1, sk_sr1, kur_sr1, 0
+    xsr_sr1, m_sr1, moda_sr1, q1_sr1, q3_sr1, var_sr1,  os_sr1_r, var_sr1_n, os_sr1_r_n, d1_sr1, d2_sr1, Q_sr1, v_sr1, vq_sr1, sk_sr1, kur_sr1, ex_sr1
   )
-  #todo linijka wyzej, eksces przedzialowy nie jest wyliczony, zalozone jest stale zero, trzeba podstawic do wzoru
   
   return (wynik_sr)
 }
-#koniec szeregu rozdzielczego
-
-
-
-
-
-
-
-
-
-
 
 oblicz_miary = function(wektor) {
   n = length(wektor)
   
   #MIARY POŁOŻENIA
-  sr = mean(wektor) #średnia
-  med = median(wektor) #mediana
+  sr = mean(wektor)
+  med = median(wektor)
   
   ux = unique(wektor)
-  moda = ux[which.max(tabulate(match(wektor, ux)))] #moda
+  moda = ux[which.max(tabulate(match(wektor, ux)))]
   
-  # Podobno type = 6 jest typowym używanym w statystyce i jest dokładniejszy niż basic (7) z excela
   q_wynik = quantile(wektor, probs = c(0.25, 0.75), type = 6)
-  q1 = q_wynik[1] #Q1
-  q3 = q_wynik[2] #Q3
+  q1 = q_wynik[1]
+  q3 = q_wynik[2]
   
   #MIARY ROZPROSZENIA
+  war_proba = var(wektor)
+  sd_proba = sd(wektor)
   
-  # Wariancja i odchylenie Z PRÓBY (S*^2, S*) 
-  war_proba = var(wektor) # S*^2 
-  sd_proba = sd(wektor)   # S*
+  war_populacja = sum((wektor - sr)^2) / n
+  sd_populacja = sqrt(war_populacja)
   
-  # Wariancja i odchylenie Z POPULACJI (S^2, S) - bez gwiazdki
-  war_populacja = sum((wektor - sr)^2) / n # S^2 
-  sd_populacja = sqrt(war_populacja)       # S
-  d1 = sum(abs(wektor - sr)) / n # odchylenie przeciętne od średniej
-  d2 = sum(abs(wektor - med)) / n # odchylenie przeciętne od mediany
-  odchylenie = sd_populacja #JAK COŚ JEST ŹLE LUB ZŁY TYP ODCHYLENIA DO OBLICZEŃ TO TU ZMIENIĆ między "sd_proba" a "sd_populacja"
-  q = (q3 - q1) / 2 # odchylenie ćwiartkowe
-  v = (odchylenie / sr) * 100 # współczynnik zmienności [%]
-  vq = (q / med) * 100 # pozycyjny współczynnik zmienności [%]
+  d1 = sum(abs(wektor - sr)) / n
+  d2 = sum(abs(wektor - med)) / n
+  
+  odchylenie = sd_populacja
+  q = (q3 - q1) / 2
+  v = (odchylenie / sr) * 100
+  vq = (q / med) * 100
   
   #MIARY ASYMETRII I KONCENTRACJI
-  
   m3 = sum((wektor - sr)^3) / n
   m4 = sum((wektor - sr)^4) / n
   
-  as = m3 / (odchylenie^3) # skośność
-  krt = m4 / (odchylenie^4) # kurtoza
-  g2 = krt - 3 # eksces
+  as = m3 / (odchylenie^3)
+  krt = m4 / (odchylenie^4)
+  g2 = krt - 3
   
-  #ZAPIS + OPIS WYNIKÓW
   wyniki = c(
     "Średnia:" = sr,
     "Mediana" = med,
@@ -186,8 +153,6 @@ oblicz_miary = function(wektor) {
     "Eksces g2" = g2
   )
   
-  
-  #łączenie podpunktów a i b
   szer_rozdz = wart_sr(wektor)
   
   tabela_wynikow = data.frame(
@@ -196,7 +161,5 @@ oblicz_miary = function(wektor) {
     wart_szer_rozdziel = as.numeric(round(szer_rozdz, 4))
   )
   
-  
   return(tabela_wynikow)
-  
 }
